63267477399a183ab10d92d7c29537ac6bdeedcd
TeoNemsadze <teona.nemsadze.2@iliauni.edu.ge>
2023-11-09 12:15:06 +0400

    Initial Commit

---
+++ BarCodeScanner/BarCodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarCodeScannerApp.swift
+//  BarCodeScanner
+//
+//  Created by teona nemsadze on 09.11.23.
+//
+
+import SwiftUI
+
+@main
+struct BarCodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
